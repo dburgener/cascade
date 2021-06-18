94d80aa70681e9cc9aaff5281d287f920b5347af
Daniel Burgener <Daniel.Burgener@microsoft.com>
2021-06-18 09:44:16 -0400

    Add plumbing to link the library and hllc together

--- src/bin/hllc.rs
+++ src/bin/hllc.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello World!");
+use SELinuxhll::compile_system_policy;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process::exit;
+
+fn usage() {
+    println!("hllc policy.hll");
+}
+
+fn main() -> std::io::Result<()> {
+    // TODO: Move all this to a parse args function
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        usage();
+        exit(1);
+    }
+
+    let mut policies: Vec<&mut File> = Vec::new();
+    let mut in_file = File::open(&args[1])?;
+    policies.push(&mut in_file);
+
+    let mut out_file = File::create("out.cil")?;
+    compile_system_policy(policies, &mut out_file);
+
+    return Ok(());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,66 @@ extern crate lalrpop_util;
 
 mod ast;
 
+use std::fs::{File};
+use std::error::Error;
+use std::io::{Error as IOError, Read, Write};
+use std::fmt;
+
+lalrpop_mod!(pub parser);
+
+#[derive(Clone, Debug)]
+struct HLLCompileError {}
+
+impl fmt::Display for HLLCompileError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "TODO")
+    }
+}
+
+impl Error for HLLCompileError {}
+
+// TODO: Should use a more specific error type
+pub fn compile_system_policy(input_files: Vec<&mut File>, out_file: &mut File) -> Result<(), Box<dyn Error>> {
+    let mut policies: Vec<Box<ast::Policy>> = Vec::new();
+    for f in input_files {
+        let mut policy_str = String::new();
+        f.read_to_string(&mut policy_str)?;
+        let p = parse_policy(&policy_str);
+        let p = match p {
+            Ok(p) => p,
+            Err(e) => { println!("TODO: Handle parse errors cleanly");
+                return Err(Box::new(HLLCompileError {}));
+            }
+        };
+
+        policies.push(p);
+    }
+
+    // TODO: Combine multiple files
+    // TODO: It would be so wonderful if we could gaurantee that this can't fail.  Is that
+    // possible?
+    let out_str = generate_cil(&*policies[0]);
+
+    write_out_cil(out_file, out_str)?;
+
+    return Ok(());
+}
+
+fn parse_policy<'a>(policy: &'a str) -> Result<Box<ast::Policy>, lalrpop_util::ParseError<usize, lalrpop_util::lexer::Token<'a>, &'static str>> {
+    // TODO: Probably should only construct once
+    // Why though?
+    return parser::PolicyParser::new().parse(policy);
+}
+
+// TODO: expand to multiple input files
+fn generate_cil(p: &ast::Policy) -> String{
+    return "TODO".to_string();
+}
+
+fn write_out_cil(f: &mut File, s: String) -> Result<(), IOError> {
+    f.write_all(s.as_bytes())?;
+    return Ok(());
+}
 
 #[cfg(test)]
 mod tests {
