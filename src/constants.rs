dda9e2998d94168ae93891761ac6ec0a482e64b5
Wei Liu <liuwe@microsoft.com>
2021-11-04 22:06:28 +0000

    Remove redundant static lifetime in constants.rs
    Signed-off-by: Wei Liu <liuwe@microsoft.com>

--- src/constants.rs
+++ src/constants.rs
@@ -1,20 +1,20 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: MIT
-pub const ALLOW_FUNCTION_NAME: &'static str = "allow";
-pub const DONTAUDIT_FUNCTION_NAME: &'static str = "dontaudit";
-pub const AUDITALLOW_FUNCTION_NAME: &'static str = "auditallow";
-pub const NEVERALLOW_FUNCTION_NAME: &'static str = "neverallow";
-pub const FILE_CONTEXT_FUNCTION_NAME: &'static str = "file_context";
-pub const DOMTRANS_FUNCTION_NAME: &'static str = "domain_transition";
+pub const ALLOW_FUNCTION_NAME: &str = "allow";
+pub const DONTAUDIT_FUNCTION_NAME: &str = "dontaudit";
+pub const AUDITALLOW_FUNCTION_NAME: &str = "auditallow";
+pub const NEVERALLOW_FUNCTION_NAME: &str = "neverallow";
+pub const FILE_CONTEXT_FUNCTION_NAME: &str = "file_context";
+pub const DOMTRANS_FUNCTION_NAME: &str = "domain_transition";
 
-pub const AV_RULES: &'static [&'static str] = &[
+pub const AV_RULES: &[&str] = &[
     ALLOW_FUNCTION_NAME,
     DONTAUDIT_FUNCTION_NAME,
     AUDITALLOW_FUNCTION_NAME,
     NEVERALLOW_FUNCTION_NAME,
 ];
 
-pub const BUILT_IN_TYPES: &'static [&'static str] = &[
+pub const BUILT_IN_TYPES: &[&str] = &[
     "domain",
     "resource",
     "path",
