c6b1fc4397bf29df10928b88c1ad2d03685ff978
Daniel Burgener <Daniel.Burgener@microsoft.com>
2022-05-03 18:56:44 -0400

    Enable recursive directory traversal in casc
    This enables recursively discovering all policy files in the specified
    path(s), which is expected to be the most common use case.
    
    Additional ergonomics, like defaulting to CWD if no path is specified is
    future work.

--- src/bin/casc.rs
+++ src/bin/casc.rs
@@ -6,19 +6,40 @@ use selinux_cascade::error::ErrorItem;
 use clap::Parser;
 use std::fs::File;
 use std::io::{Error, ErrorKind, Write};
+use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
-#[clap(author, version, name = "casc")]
+#[clap(author, version, name = "casc", about)]
 struct Args {
+    /// List of input files to process.  Directories are searched recursively.
     #[clap(required(true))]
     input_file: Vec<String>,
 }
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    let policies: Vec<&str> = args.input_file.iter().map(|s| s as &str).collect();
+    let policies: Vec<String> = match get_policy_files(args.input_file) {
+        Ok(mut s) => {
+            // Always treat files in the same order for determinism in compilation
+            // sort_unstable() does not preserve equality, which is fine because two
+            // different files cannot have the same relative path
+            s.sort_unstable();
+            s
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+            return Err(e);
+        }
+    };
+    if policies.is_empty() {
+        // Files supplied on command line, but no .cas files found
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            "No policy source files found",
+        ));
+    }
     let mut out_file = File::create("out.cil")?;
-    let res = compile_system_policy(policies);
+    let res = compile_system_policy(policies.iter().map(|s| s as &str).collect());
     match res {
         Err(error_list) => {
             for e in error_list {
@@ -34,4 +55,20 @@ fn main() -> std::io::Result<()> {
         }
         Ok(s) => out_file.write_all(s.as_bytes()),
     }
+}
+
+// Create a list of policy files
+fn get_policy_files(filenames: Vec<String>) -> Result<Vec<String>, Error> {
+    let mut policy_files = Vec::new();
+    for file in filenames {
+        for entry in WalkDir::new(file) {
+            let entry = entry?;
+            if entry.file_type().is_file() && entry.path().extension().unwrap_or_default() == "cas"
+            {
+                let filename = entry.path().display().to_string();
+                policy_files.push(filename);
+            }
+        }
+    }
+    Ok(policy_files)
 }
