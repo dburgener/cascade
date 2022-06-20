86c536d481e14505674cff6457b3e473ae061c2a
Daniel Burgener <Daniel.Burgener@Microsoft.com>
2022-06-20 09:42:50 -0400

    Add a basic man page

--- build.rs
+++ build.rs
@@ -1,5 +1,24 @@
 extern crate lalrpop;
 
-fn main() {
+use clap::CommandFactory;
+
+#[path = "src/bin/casc/args.rs"]
+mod casc;
+
+fn main() -> std::io::Result<()> {
+    // Generate parser
     lalrpop::process_root().unwrap();
+
+    // Generate man page
+    // https://rust-cli.github.io/book/in-depth/docs.html
+    let out_dir =
+        std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
+    let cmd = casc::Args::command();
+    let man = clap_mangen::Man::new(cmd);
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
+
+    std::fs::write(out_dir.join("casc.1"), buffer)?;
+
+    Ok(())
 }
