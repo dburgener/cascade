d8b840aba134500e62bada4cd2ed08c080ae9a8e
Daniel Burgener <Daniel.Burgener@microsoft.com>
2022-11-17 17:05:31 -0500

    Store strings in domtrans rules instead of TypeInfos
    This allows them to work correctly with @derive and function calls.
    Otherwise they 1. Must be a valid type (not an argument name) and 2.
    Will always be that type (even if derived or called with some type as an
    argument).

--- src/lib.rs
+++ src/lib.rs
@@ -832,7 +832,8 @@ mod tests {
         "(macro custom_define-read ((type this) (type source)) (allow source this (lnk_file (read))))",
         "(macro derive_all-read ((type this) (type source)) (allow source this (dir (read))) (allow source this (file (read))))",
         "(macro derive_all-write ((type this) (type source)) (allow source this (dir (write))))",
-        "(call associates-to_associate-some_associated_call"],
+        "(call associates-to_associate-some_associated_call",
+        "(macro some_child-domtrans ((type this) (type source) (type exec)) (typetransition source exec process this))"],
         &[]);
     }
 
