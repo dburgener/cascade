011351ec55e016d1f2d03a3e78f65a72b19499a2
Daniel Burgener <Daniel.Burgener@microsoft.com>
2022-12-02 10:35:09 -0500

    Split github actions into separate jobs
    Build: Simple compile check first.  Don't run anything else if it
    doesn't compile
    Checks: Various sanity checks (clippy, fmt etc)
    Tests: Run cargo test
    
    The main advantage here is that we will be able to run the tests in a
    matrix to test different secilc versions in parallel

---
+++ .github/workflows/restore-nightly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+# https://nickb.dev/blog/azure-pipelines-for-rust-projects
+curl --proto '=https' -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="${PATH}:${HOME}/.cargo/bin"
+echo "##vso[task.setvariable variable=PATH;]${PATH}"
+
+rustup toolchain install nightly
+
