ea9b5252d0ba4c43e48dc595bf9e5705de88fcaf
Daniel Burgener <Daniel.Burgener@microsoft.com>
2022-12-02 10:35:09 -0500

    Split github actions into separate jobs
    Build: Simple compile check first.  Don't run anything else if it
    doesn't compile
    Checks: Various sanity checks (clippy, fmt etc)
    Tests: Run cargo test
    
    The main advantage here is that we will be able to run the tests in a
    matrix to test different secilc versions in parallel

--- .github/workflows/restore-test.sh
+++ .github/workflows/restore-test.sh
@@ -2,13 +2,6 @@
 
 set -e -u -o pipefail
 
-# https://nickb.dev/blog/azure-pipelines-for-rust-projects
-curl --proto '=https' -sSf https://sh.rustup.rs | sh -s -- -y
-export PATH="${PATH}:${HOME}/.cargo/bin"
-echo "##vso[task.setvariable variable=PATH;]${PATH}"
-
-rustup toolchain install nightly
-
 # Manually build secilc, rather than take the packaged version, so we can have control over version
 # Github actions will have already checked out the repo to the correct tag for this run
 sudo apt install -y --no-install-recommends flex bison pkg-config libaudit-dev libbz2-dev libustr-dev libpcre3-dev xmlto
