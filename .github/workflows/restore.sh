c34515b7df74e959c9c08977f490812b56f3f9c8
Daniel Burgener <Daniel.Burgener@microsoft.com>
2022-12-02 10:16:00 -0500

    Build libsepol and secilc manually instead of using packaged version
    This will allow us flexibility to test against multiple secilc versions

--- .github/workflows/restore.sh
+++ .github/workflows/restore.sh
@@ -9,4 +9,11 @@ echo "##vso[task.setvariable variable=PATH;]${PATH}"
 
 rustup toolchain install nightly
 
-sudo apt-get -y install secilc
+# Manually build secilc, rather than take the packaged version, so we can have control over version
+# Github actions will have already checked out the repo to the correct tag for this run
+sudo apt install -y --no-install-recommends flex bison pkg-config libaudit-dev libbz2-dev libustr-dev libpcre3-dev xmlto
+
+pushd selinux
+
+sudo make -j16 -C libsepol install
+sudo make -j16 -C secilc install
