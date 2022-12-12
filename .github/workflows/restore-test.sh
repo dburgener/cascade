1acef0a8c8ff1d66f38c250131230c9b1a62f282
Daniel Burgener <Daniel.Burgener@microsoft.com>
2022-12-12 11:08:14 -0500

    Refresh package info prior to fetching
    Can help if packages have moved

--- .github/workflows/restore-test.sh
+++ .github/workflows/restore-test.sh
@@ -4,6 +4,7 @@ set -e -u -o pipefail
 
 # Manually build secilc, rather than take the packaged version, so we can have control over version
 # Github actions will have already checked out the repo to the correct tag for this run
+sudo apt update
 sudo apt install -y --no-install-recommends flex bison pkg-config libaudit-dev libbz2-dev libustr-dev libpcre3-dev xmlto
 
 pushd selinux
