c4e21a7e3bddcad61647b93a2224235d16e5dd01
Matt Sheets <masheets@linux.microsoft.com>
2023-02-08 23:03:57 -0700

    fix matrix?

--- .github/workflows/clippy.sh
+++ .github/workflows/clippy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# -e is not being set here since we need to store the return of
-# this script as part of the pipeline
-set -u -o pipefail
+set -e -u -o pipefail
 
 VERSION=${1:-stable}
 
