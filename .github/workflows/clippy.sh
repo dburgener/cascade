a0d9c0d3e37d1dc1f641aa1323710882879207e8
Daniel Burgener <Daniel.Burgener@microsoft.com>
2023-04-07 11:34:30 -0400

    Ignore clippy manual_flatten errors
    1. IMHO this suggestion typically makes the code less readable.  I find
    the interior if logic clearer than locating a flatten() in the middle of
    a chained collection of iterator calls and recalling the semantics of
    flatten() on an iterator.
    2. The code suggestions provided by clippy to fix this issue do not
    always seem to compile.  I got #159 to compile and make clippy happen,
    and the flattening feels readable enough there, but the finally
    compiling code does not match clippy's suggestion, which was broken.
    
    (In #159 I appeared to be running into something essentially similar to
    https://github.com/rust-lang/rust-clippy/issues/6893, but according to
    that issue, it was fixes a few years ago, so I'm not sure what's going
    on.  I might dig into it more, but as mentioned in point 1, I'm
    skeptical of the value of this check anyways)

--- .github/workflows/clippy.sh
+++ .github/workflows/clippy.sh
@@ -4,7 +4,7 @@ set -e -u -o pipefail
 
 VERSION=${1:-stable}
 
-rustup run ${VERSION} cargo clippy -- -A clippy::new_without_default --deny warnings
+rustup run ${VERSION} cargo clippy -- -A clippy::new_without_default -A clippy::manual_flatten --deny warnings
 
 # Suppress uninlined_format_args since assert! has a format! macro in it, which confuses clippy.
-rustup run ${VERSION} cargo clippy --tests -- -A clippy::new_without_default -A clippy::expect_fun_call -A clippy::uninlined_format_args
+rustup run ${VERSION} cargo clippy --tests -- -A clippy::new_without_default -A clippy::expect_fun_call -A clippy::uninlined_format_args -A clippy::manual_flatten
