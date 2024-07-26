aa7b254c3f5ab7609900c238b96bdb1cfb87c1ee
Daniel Burgener <Daniel.Burgener@microsoft.com>
2024-07-26 10:41:20 -0400

    Move clippy overrides from the github actions code into the source
    This creates a predictable experience for developers running "cargo
    clippy" locally.
    
    Note that "uninlined_format_args" was allowed due to a clippy bug
    which has since been fixed, so that exception is no longer needed.

--- .github/workflows/clippy.sh
+++ .github/workflows/clippy.sh
@@ -4,7 +4,6 @@ set -e -u -o pipefail
 
 VERSION=${1:-stable}
 
-rustup run ${VERSION} cargo clippy -- -A clippy::new_without_default -A clippy::manual_flatten --deny warnings
+rustup run ${VERSION} cargo clippy -- --deny warnings
 
-# Suppress uninlined_format_args since assert! has a format! macro in it, which confuses clippy.
-rustup run ${VERSION} cargo clippy --tests -- -A clippy::new_without_default -A clippy::expect_fun_call -A clippy::uninlined_format_args -A clippy::manual_flatten
+rustup run ${VERSION} cargo clippy --tests
