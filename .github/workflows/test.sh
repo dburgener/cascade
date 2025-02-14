3f69d86933c3416ed2637d61119fe493282cb160
Daniel Burgener <Daniel.Burgener@microsoft.com>
2025-02-14 13:20:00 -0500

    More debug

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -5,7 +5,7 @@ set -e -u -x -o pipefail
 VERSION=${1:-stable}
 
 echo ${PATH}
-ldd `which secilc`
+LD_LIBRARY_PATH=~/selinux:$LD_LIBRARY_PATH ldd `which secilc`
 ls /usr/local/bin
 
 LD_LIBRARY_PATH=~/selinux:$LD_LIBRARY_PATH rustup run ${VERSION} cargo test --verbose
